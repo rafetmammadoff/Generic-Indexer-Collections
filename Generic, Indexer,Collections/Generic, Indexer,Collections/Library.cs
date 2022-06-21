70b74a2862d2b0a908a85a3edc8214c818783baf
Rafet <professionalraf@gmail.com>
2022-06-21 19:50:15 +0400

    Task 2 complete

--- Generic, Indexer,Collections/Generic, Indexer,Collections/Library.cs
+++ Generic, Indexer,Collections/Generic, Indexer,Collections/Library.cs
@@ -6,6 +6,52 @@ namespace Generic__Indexer_Collections
 {
     internal class Library
     {
-        
+        List<Book> Books=new List<Book> ();
+
+        public List<Book> FindAllBooksByName(string text)
+        {
+            List <Book> newBooks = new List<Book> ();
+            for (int i = 0; i < Books.Count; i++)
+            {
+                if (Books[i].Name.Contains(text))
+                {
+                    newBooks.Add(Books[i]);
+                }
+            }
+            return newBooks;
+        }
+        public void RemoveAllBookByName(string text)
+        {
+            for (int i = 0; i < Books.Count; i++)
+            {
+                if (Books[i].Name.Contains(text))
+                {
+                    Books.Remove(Books[i]);
+                }
+            }
+        }
+        public List<Book> SearchBooks(string text)
+        {
+            List<Book> newBooks = new List<Book>();
+            for (int i = 0; i < Books.Count; i++)
+            {
+                if (Books[i].Name.Contains(text) || Books[i].AuthorName.Contains(text) || Books[i].PageCount.ToString().Contains(text))
+                {
+                    newBooks.Add(Books[i]);
+                }
+            }
+            return newBooks;
+        }
+        public void Add(Book book)
+        {
+            Books.Add(book);
+        }
+        public void ShowBook()
+        {
+            for (int i = 0; i < Books.Count; i++)
+            {
+                Console.WriteLine(Books[i].Name);
+            }
+        }
     }
 }
--- Generic, Indexer,Collections/Generic, Indexer,Collections/Program.cs
+++ Generic, Indexer,Collections/Generic, Indexer,Collections/Program.cs
@@ -7,13 +7,61 @@ namespace Generic__Indexer_Collections
     {
         static void Main(string[] args)
         {
-            Library library = new Library();
-            library.Add(new Book("Gorunmeyen izler", "Elxan Elatli", 322));
-            library.Add(new Book("tokulmeyen izler", "Elxan Elatli", 322));
-            library.Add(new Book("acilmiyan izler", "Elxan Elatli", 3232));
-            library.Add(new Book("soxulmayan izler", "Elxan Elatli", 322));
-            library.RemoveAllBookByName("aci");
-            library.ShowBook();
+            List<string> list = new List<string>();
+            list.Add("Rafet");
+            list.Add("Ehmed");
+            list.Add("Nurlan");
+            list.Add("Kenan");
+
+            list.Remove("Rafet");
+            list.RemoveAt(0);
+            list.RemoveRange(0, 1);
+            Console.WriteLine(list.Count);
+            list.ForEach(x => Console.WriteLine(x));
+            list.Contains(list[0]);
+            int[] arr=new int[] { 1, 2, 3 };
+            List<int> list2 = new List<int>(arr);
+            list2.Clear();
+
+            HashSet<int> list3 = new HashSet<int>(arr)
+            {
+                53,53,534,123
+            };
+            foreach (var item in list3)
+            {
+                Console.WriteLine(item.GetHashCode());
+            }
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Pop();
+            stack.Peek();
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(4);
+            queue.Enqueue(9);
+            queue.Dequeue();
+            queue.Peek();
+
+
+            SortedList<int,string> keyValuePairs = new SortedList<int,string>();
+            keyValuePairs.Add(1, "bir");
+            keyValuePairs.Add(2, "iki");
+            keyValuePairs.Add(3, "uc");
+
+            foreach (KeyValuePair<int,string> item in keyValuePairs)
+            {
+                Console.WriteLine("key:{0} value:{1}",item.Key,item.Value);
+            }
+            Console.WriteLine("=================");
+            Console.WriteLine(keyValuePairs[3]);
+            if (keyValuePairs.ContainsKey(2))
+            {
+                keyValuePairs[2] = "ehmed";
+            }
         }
     }
 }
